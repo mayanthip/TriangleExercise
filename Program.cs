79afbbc674197a54bc5c4d4a080661df22f09011
Mayanthi Perera <mayanthi@netwealth.com.au>
2018-11-01 10:20:56 +1100

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Triangle2
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+           
+           for (int i=1; i<=5; i++)
+           {
+               int j=1;
+               while (j<=i)
+
+               {
+                   Console.Write("*");
+                    j++;
+               }
+                Console.WriteLine("\n");
+           }
+        }
+    }
+}
